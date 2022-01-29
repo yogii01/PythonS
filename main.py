afb885e36539864c32f003c341e3a3336ebbf871
yogii01 <66463213+yogii01@users.noreply.github.com>
2022-01-29 16:39:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
-import os, time
+import os, datetime
 
-path = r"D:\Sample"
-now = time.time()
 
-for filename in os.listdir(path):
-    # if os.stat(os.path.join(path, filename)).st_mtime < now - 7 * 86400:
-    if os.path.getmtime(os.path.join(path, filename)) < now - 7 * 86400:
-        if os.path.isfile(os.path.join(path, filename)):
-            print(filename)
-            os.remove(os.path.join(path, filename))
+
+
+path = 'G:\py'
+os.chdir(path)
+files= os.getcwd()
+files = os.listdir(files)
+files = sorted(files,key=os.path.getmtime)
+latest = files.reverse()
+
+for file in files[5:]:
+    
+    os.remove(os.path.join(path, file))
+
+for file in files[:5]:
+
+    print(file)
+
+
+
+
 
 import os
 from pathlib import Path
 from zipfile import ZipFile
 
 
-DOWNLOAD_DIR = Path("D:\Sample")
-ZIPPED_FILE_DIR = Path("D:\Sample")
+DOWNLOAD_DIR = Path("G:\py")
+ZIPPED_FILE_DIR = Path("G:\py")
 
 
 def get_list_of_all_folders(download_dir: Path):
@@ -31,4 +43,4 @@ def zip_files():
             zip.write(folder)
 
 
-zip_files()
+zip_files()   
